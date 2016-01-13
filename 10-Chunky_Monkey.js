db46a75e7a6d270a7ce08c88134e96d47ab78427
Livia Dobai <livia.dobai@gmail.com>
2016-01-14 00:02:21 +0100

    added scripts 9 and 10

---
+++ 10-Chunky_Monkey.js
@@ -0,0 +1,30 @@
+/*Chunky Monkey
+Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a multidimensional array.
+*/
+
+function chunk(arr, size) {
+  // Break it up.
+   var newArray = [];
+  for(var i=0;i<arr.length;i+=size) {
+    var subArray = [];
+    for(var j=0;j<size;j++) {
+      if(arr[i+j] !== undefined) {
+        subArray.push(arr[i+j]);
+      }
+    }
+    newArray.push(subArray);
+  }
+  arr = newArray;
+  console.log(arr);
+  return arr;
+}
+
+chunk(["a", "b", "c", "d"], 2);
+
+//TESTS
+//chunk(["a", "b", "c", "d"], 2) should return [["a", "b"], ["c", "d"]].
+//chunk([0, 1, 2, 3, 4, 5], 3) should return [[0, 1, 2], [3, 4, 5]].
+//chunk([0, 1, 2, 3, 4, 5], 2) should return [[0, 1], [2, 3], [4, 5]].
+//chunk([0, 1, 2, 3, 4, 5], 4) should return [[0, 1, 2, 3], [4, 5]].
+//chunk([0, 1, 2, 3, 4, 5, 6], 3) should return [[0, 1, 2], [3, 4, 5], [6]].
+//chunk([0, 1, 2, 3, 4, 5, 6, 7, 8], 4) should return [[0, 1, 2, 3], [4, 5, 6, 7], [8]].
